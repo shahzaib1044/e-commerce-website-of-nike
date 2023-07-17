c4badf31c127541731c67c763051d32887145567
shahzaib <shahzebraheel61@gmail.com>
2023-07-17 08:39:08 +0500

    header and footer adjusted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Icon } from '@mui/material';
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+import './footer.css';
 
 const Footer = () => {
   const footerStyle = {
     backgroundColor: '#333',
     color: '#fff',
     padding: '20px',
-    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   };
 
   const paragraphStyle = {
@@ -14,6 +19,23 @@ const Footer = () => {
 
   return (
     <footer style={footerStyle}>
+      <div className="footer-links">
+        <a href="/">Home</a>
+        <a href="/Products">Products</a>
+        <a href="/Aboutus">About</a>
+        <a href="/contactus">Contact</a>
+      </div>
+      <div className="social-icons">
+        <Icon className="icon">
+          <FaFacebook />
+        </Icon>
+        <Icon className="icon">
+          <FaInstagram />
+        </Icon>
+        <Icon className="icon">
+          <FaYoutube />
+        </Icon>
+      </div>
       <p style={paragraphStyle}>&copy; {new Date().getFullYear()} Nike. All rights reserved.</p>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,152 +1,202 @@
-import React,{ useContext }  from 'react';
-import { Icon } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { AuthContext } from './AuthContext';
+// import React,{ useContext }  from 'react';
+// import { Icon } from '@mui/material';
+// import SearchIcon from '@mui/icons-material/Search';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import { AuthContext } from './AuthContext';
 
-const Header = () => {
+// const Header = () => {
+//   const { loggedInUser } = useContext(AuthContext);
+//   const headerStyle = {
+//     backgroundColor: '#111',
+//     color: '#fff',
+//     padding: '20px',
+//   };
+
+//   const searchContainer = {
+//     display: 'flex',
+//     alignItems: 'center',
+//     marginTop: '1px',
+//     marginBottom: '1px',
+//     backgroundColor: 'rgba(59,70,88,255)',
+//     padding: '4px',
+//     borderRadius: '4px',
+//   };
+
+//   const linkStyle = {
+//     color: '#fff',
+//     textDecoration: 'none',
+//     fontWeight: 'bold',
+//     fontSize: '20px',
+//     marginLeft: '50px',
+//     marginRight: '20px',
+//   };
+
+//   const linkStyle1 = {
+//     color: '#fff',
+//     textDecoration: 'none',
+//     fontWeight: 'bold',
+//     fontSize: '20px',
+//     marginLeft: '50px',
+//     position: 'relative',
+//     top: '44px',
+//   };
+
+//   const inputContainerStyle = {
+//     display: 'flex',
+//     alignItems: 'center',
+//   };
+
+//   const ulStyle = {
+//     listStyle: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     marginLeft: '1400px',
+//     marginBottom: '30px',
+//     position: 'relative',
+//     top: '-45px',
+//   };
+
+//   const ulStyle3 = {
+//     listStyle: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     marginLeft: '220px',
+//     position: 'relative',
+//     top: '-20px',
+//   };
+
+//   const searchIconStyle = {
+//     marginRight: '10px',
+//   };
+
+//   const iconButtonStyle = {
+//     marginLeft: '10px',
+//   };
+
+//   const logo = require("./downloa.png");
+
+//   const userCircleStyle = {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     width: '40px',
+//     height: '40px',
+//     borderRadius: '50%',
+//     backgroundColor: '#fff',
+//     color: '#000',
+//     fontWeight: 'bold',
+//     fontSize: '18px',
+//   };
+
+//   return (
+//     <header style={headerStyle}>
+//       <nav>
+//         <a href="/" style={linkStyle1}>
+//           <img src={logo} alt="Nike logo" height="70" width="150" />
+//         </a>
+//         <ul>
+//           <div style={ulStyle3}>
+//             <li>
+//               <a href="/" style={linkStyle}>
+//                 Home
+//               </a>
+//             </li>
+//             <li>
+//               <a href="/Products" style={linkStyle}>
+//                 Products
+//               </a>
+//             </li>
+//             <li>
+//               <a href="/Aboutus" style={linkStyle}>
+//                 About
+//               </a>
+//             </li>
+//             <li>
+//               <a href="/contactus" style={linkStyle}>
+//                 Contact
+//               </a>
+//             </li>
+//           </div>
+//           <div style={ulStyle}>
+//             <div style={searchContainer}>
+//               <SearchIcon />
+//               <input type="text" placeholder="Search here..." />
+//             </div>
+//             <li>
+//               <Icon style={linkStyle}>
+//                 <FavoriteIcon />
+//               </Icon>
+//             </li>
+//             <li>
+//               <Icon style={linkStyle}>
+//                 <ShoppingCartIcon />
+//               </Icon>
+//             </li>
+//             {loggedInUser ? (
+//               <li>
+//                 <div style={userCircleStyle}>{loggedInUser.charAt(0).toUpperCase()}</div>
+//               </li>
+//             ) : (
+//               <li>
+//                 <a href="/Signin" style={linkStyle}>
+//                   Signin
+//                 </a>
+//               </li>
+//             )}
+//           </div>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+import { useRef, useState,useContext } from "react";
+import { Icon, InputBase } from "@mui/material";
+import { FaBars, FaTimes } from "react-icons/fa";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { AuthContext } from "./AuthContext";
+import "./head.css";
+
+function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const { loggedInUser } = useContext(AuthContext);
-  const headerStyle = {
-    backgroundColor: '#111',
-    color: '#fff',
-    padding: '1vh',
-  };
-
-  const searchContainer = {
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: 'rgba(59,70,88,255)',
-    padding: '1vh',
-    borderRadius: '0.5vh',
-  };
-
-  const linkStyle = {
-    color: '#fff',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    fontSize: '2.5vh', // Converted from '20px'
-    marginLeft: '3.33vh', // Converted from '50px'
-    marginRight: '1.33vh', // Converted from '20px'
-  };
-
-  const linkStyle1 = {
-    color: '#fff',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    fontSize: '1.5vh', // Converted from '20px'
-    marginLeft: '3.33vh', // Converted from '50px'
-    position: 'relative',
-    top: '6.67vh', // Converted from '44px'
-  };
-
-  const inputContainerStyle = {
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-  const ulStyle = {
-    listStyle: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '153.33vh', // Converted from '1400px'
-    marginBottom: '3vh', // Converted from '30px'
-    position: 'relative',
-    top: '-4.5vh', // Converted from '-45px'
-  };
-
-  const ulStyle3 = {
-    listStyle: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '14.67vh', // Converted from '220px'
-    position: 'relative',
-    top: '-0.67vh', // Converted from '-20px'
-  };
-
-  const searchIconStyle = {
-    marginRight: '1.33vh', // Converted from '10px'
-  };
-
-  const iconButtonStyle = {
-    marginLeft: '1.33vh', // Converted from '10px'
-  };
-
   const logo = require("./downloa.png");
 
-  const userCircleStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '2.67vh', // Converted from '40px'
-    height: '2.67vh', // Converted from '40px'
-    borderRadius: '3.33vh', // Converted from '50%'
-    backgroundColor: '#fff',
-    color: '#000',
-    fontWeight: 'bold',
-    fontSize: '1.2vh', // Converted from '18px'
-  }
+  const showNavbar = () => {
+    setIsNavOpen(!isNavOpen);
+  };
 
   return (
-    <header style={headerStyle}>
-      <nav>
-        <a href="/" style={linkStyle1}>
-          <img src={logo} alt="Nike logo" height="70" width="150" />
-        </a>
-        <ul>
-          <div style={ulStyle3}>
-            <li>
-              <a href="/" style={linkStyle}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/Products" style={linkStyle}>
-                Products
-              </a>
-            </li>
-            <li>
-              <a href="/Aboutus" style={linkStyle}>
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/contactus" style={linkStyle}>
-                Contact
-              </a>
-            </li>
-          </div>
-          <div style={ulStyle}>
-            <div style={searchContainer}>
-              <SearchIcon />
-              <input type="text" placeholder="Search here..." />
-            </div>
-            <li>
-              <Icon style={linkStyle}>
-                <FavoriteIcon />
-              </Icon>
-            </li>
-            <li>
-              <Icon style={linkStyle}>
-                <ShoppingCartIcon />
-              </Icon>
-            </li>
-            {loggedInUser ? (
-              <li>
-                <div style={userCircleStyle}>{loggedInUser.charAt(0).toUpperCase()}</div>
-              </li>
-            ) : (
-              <li>
-                <a href="/Signin" style={linkStyle}>
-                  Signin
-                </a>
-              </li>
-            )}
-          </div>
-        </ul>
+    <header>
+      <img src={logo} alt="Nike logo" height="50" width="120" />
+      <nav className={isNavOpen ? "responsive_nav" : ""}>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+        <a href="/">Home</a>
+        <a href="/Products">Products</a>
+        <a href="/Aboutus">About</a>
+        <a href="/contactus">Contact</a>
+        <div className="search-container">
+          
+          {loggedInUser ? (
+            <div className="user-circle">{loggedInUser.charAt(0).toUpperCase()}</div>
+          ) : (
+            <a href="/Signin">Signin/Signup</a>
+          )}
+        </div>
       </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+      
+        
     </header>
   );
-};
+}
 
 export default Header;
